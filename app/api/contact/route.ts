064f7aac53b60efe33f28baec21f9ee29e402112
pairploy-ch <pairploy.chp@gmail.com>
2026-02-22 17:57:23 +0700

    phone

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('JucqCMTA_P57TPMbACAGZcMpECbPfzD8E');
 
 export async function POST(req: Request) {
   try {
